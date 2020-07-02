c0fec19298ef78f7a8f3eaa73445b832159e3069
Nguyen Quoc Hai <harisato@harisatos-Mac.local>
2020-07-02 14:11:20 +0700

    Add more color to ultis.sh

--- utils.sh
+++ utils.sh
@@ -3,3 +3,15 @@ function error() {
     COLOR_RED="$(tput setaf 1)"
     printf "%s%s%s\n" $COLOR_RED "$1" $COLOR_REST
 }
+
+function valid() {
+    COLOR_REST="$(tput sgr0)"
+    COLOR_RED="$(tput setaf 2)"
+    printf "%s%s%s\n" $COLOR_RED "$1" $COLOR_REST
+}
+
+function invalid() {
+    COLOR_REST="$(tput sgr0)"
+    COLOR_RED="$(tput setaf 3)"
+    printf "%s%s%s\n" $COLOR_RED "$1" $COLOR_REST
+}
